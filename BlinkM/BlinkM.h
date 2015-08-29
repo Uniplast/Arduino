29ed9764a6da1134a895d7ed283aac9ac0df9000
Uniplast <Uniplast@users.noreply.github.com>
2015-08-29 04:05:25 -0700

    Update BlinkM.h

--- BlinkM/BlinkM.h
+++ BlinkM/BlinkM.h
@@ -15,22 +15,39 @@ class BlinkM {
 public:
 	BlinkM();	//Default constructor.
 	void setRGB(uint8_t addr, uint8_t R, uint8_t G, uint8_t B);
+
 	void fadeRGB(uint8_t addr, uint8_t R, uint8_t G, uint8_t B);
+
 	void fadeHSB(uint8_t addr, uint8_t H, uint8_t S, uint8_t B);
+
 	void fadeRandRGB(uint8_t addr, uint8_t R, uint8_t G, uint8_t B);
+
 	void fadeRandHSB(uint8_t addr, uint8_t H, uint8_t S, uint8_t B);
+
 	void playScript(uint8_t addr, uint8_t scriptId, uint8_t rpt, uint8_t line);
+
 	void stopScript(uint8_t addr);
+
 	void setFadeSpeed(uint8_t addr, uint8_t speed);
+
 	void setTimeAdjust(uint8_t addr, uint8_t time);
+
 	void getCurrentRGB(int addr, uint8_t *R, uint8_t *G, uint8_t *B);
+
 	void readScriptLine(int addr, uint8_t scriptId, uint8_t line, uint8_t *duration, uint8_t *command, uint8_t *arg1, uint8_t *arg2, uint8_t *arg3);
+
 	void setScriptLengthRpt(uint8_t addr, uint8_t scriptId, uint8_t len, uint8_t rpt);
+
 	void setLightAddress(uint8_t addr, uint8_t newAddr);
+
 	void getLightAddress(uint8_t *addr);
+
 	void getFirmwareVersion(int addr, uint8_t *majorVersion, uint8_t *minorVersion);
+
 	void setStartParams(uint8_t addr, uint8_t mode, uint8_t scriptId, uint8_t rpt, uint8_t fadeSpeed, uint8_t timeAdj);
+
 	void scanAddresses();
+
 	void scanAddresses(int scanDelay);
 private:
 
